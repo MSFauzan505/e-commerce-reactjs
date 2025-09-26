391ff145e6621939d16db7ee569160f453f9b857
fauzan <msh.fauzan@gmail.com>
2025-09-26 08:57:51 +0700

    feat: carousel on page home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,48 @@
-
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Carousel
+        className="mt-5 mx-3 "
+        plugins={[
+          Autoplay({
+            delay: 5000, // 5 detik
+            stopOnInteraction: false, // tetap jalan walau user interaksi
+          }),
+        ]}
+      >
+        <CarouselContent>
+          <CarouselItem className="h-96 w-full ">
+            <img
+              src="/images/hero-image1.jpg"
+              alt="Hero"
+              className="w-full h-full object-cover object-center rounded-xl"
+            />
+          </CarouselItem>
+          <CarouselItem className="h-96 w-full">
+            <img
+              src="/images/hero-image2.jpg"
+              alt="Hero"
+              className="w-full h-full object-cover object-center rounded-xl"
+            />
+          </CarouselItem>
+          <CarouselItem className="h-96 w-full">
+            <img
+              src="/images/hero-image3.jpg"
+              alt="Hero"
+              className="w-full h-full object-cover object-center rounded-xl"
+            />
+          </CarouselItem>
+        </CarouselContent>
+      </Carousel>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
